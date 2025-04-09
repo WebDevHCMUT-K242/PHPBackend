d81588eb8628495cb69af363508d2c9f295a6db4
nghiaho310pf <>
2025-04-09 13:58:30 +0700

    initial

---
+++ common/userdata.php
@@ -0,0 +1,15 @@
+<?php
+class UserData
+{
+    public $id;
+    public $name;
+
+    function set_id($id) {
+        $this->id = $id;
+    }
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+}
+?>
